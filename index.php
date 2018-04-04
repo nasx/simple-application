2572729b5ea0fdfb0e5b234e3d3df0185595f1f6
Chris Keller <ckeller@redhat.com>
2018-04-04 09:52:38 -0400

    Update.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Hello!</title>
 </head>
 <body>
-<h1>You are connected to <?php echo $_SERVER['SERVER_NAME']; ?>!</h1>
+<h1>You are connected to <?php echo $_SERVER['SERVER_NAME']; ?> (<?php echo $_SERVER['SERVER_ADDR']; ?>)!</h1>
 </body>
 </html>
